faa3934ae596aee2a67101857e14dd67fe568ba5
Arjun P. Rosell <rosellarjun@gmail.com>
2024-11-10 19:00:59 +0800

    add link

--- resources/views/components/company-name.blade.php
+++ resources/views/components/company-name.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('/') }}" class="mr-4 flex items-center justify-between">
+<a href="/" class="mr-4 flex items-center justify-between">
     <span
         class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"
     >
