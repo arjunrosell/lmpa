2861e7f6c61214a93ec4f81786d608cbd2a04045
Arjun P. Rosell <rosellarjun@gmail.com>
2024-11-11 18:25:06 +0800

    fixed login 419 expired

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,4 +52,18 @@
             </div>
         </div>
     </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            function handleButtonClick(buttonId) {
+                const submitButton = document.getElementById(buttonId)
+                if (submitButton) {
+                    submitButton.addEventListener('click', function () {
+                        submitButton.disabled = true
+                        submitButton.closest('form').submit()
+                    })
+                }
+            }
+            handleButtonClick('loginButton')
+        })
+    </script>
 </x-layout>
