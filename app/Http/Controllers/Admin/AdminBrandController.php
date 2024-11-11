c264106cd84e31017c9122bcd582a62fced18a5c
Arjun P. Rosell <rosellarjun@gmail.com>
2024-11-11 14:11:30 +0800

    add sort and filter by brand button

--- app/Http/Controllers/Admin/AdminBrandController.php
+++ app/Http/Controllers/Admin/AdminBrandController.php
@@ -13,19 +13,50 @@ class AdminBrandController extends Controller
 {
     private function search(SearchRequest $request)
     {
-        $query = Brand::orderBy('updated_at', 'desc');
+        $query = Brand::query();
+
         if ($request->has('search')) {
             $search = $request->input('search');
             $query->where('name', 'LIKE', "%{$search}%")
                 ->orWhere('description', 'LIKE', "%{$search}%");
         }
+
+        if ($request->has('sort')) {
+            $sort = $request->input('sort');
+            switch ($sort) {
+                case 'name_asc':
+                    $query->orderBy('name', 'asc');
+                    break;
+                case 'name_desc':
+                    $query->orderBy('name', 'desc');
+                    break;
+                case 'created_asc':
+                    $query->orderBy('created_at', 'asc');
+                    break;
+                case 'created_desc':
+                    $query->orderBy('created_at', 'desc');
+                    break;
+                default:
+                    $query->orderBy('updated_at', 'desc');
+                    break;
+            }
+        } else {
+            $query->orderBy('updated_at', 'desc');
+        }
+
+        if ($request->filled('brand')) {
+            $query->where('id', $request->input('brand'));
+        }
+
         return $query->paginate(15);
     }
 
     public function index(SearchRequest $request)
     {
         $brands = $this->search($request);
-        return view('admin.brands.index', compact('brands'));
+        $allBrands = Brand::all();
+
+        return view('admin.brands.index', compact('brands', 'allBrands'));
     }
 
     public function create()
