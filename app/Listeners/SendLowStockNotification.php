0a752f1d5580b26d1d273305c181e53d31988b92
Arjun P. Rosell <rosellarjun@gmail.com>
2024-11-25 12:39:51 +0800

    replace resend API

--- app/Listeners/SendLowStockNotification.php
+++ app/Listeners/SendLowStockNotification.php
@@ -22,11 +22,19 @@ class SendLowStockNotification
      */
     public function handle(LowStock $event): void
     {
+        // $product = $event->product;
+        // $supplierEmail = $product->supplier->email;
+
+        // Mail::send('mail.low_stock', ['product' => $product], function ($message) use ($supplierEmail) {
+        //     $message->to($supplierEmail)
+        //         ->subject('Low Stock Alert');
+        // });
         $product = $event->product;
         $supplierEmail = $product->supplier->email;
 
         Mail::send('mail.low_stock', ['product' => $product], function ($message) use ($supplierEmail) {
-            $message->to($supplierEmail)
+            $message->from('alerts@lmpa.shop', 'Lister Motor Parts & Accessories')
+                ->to($supplierEmail)
                 ->subject('Low Stock Alert');
         });
     }
