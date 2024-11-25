dc669b78c1cc5318500d6c89ecf7e97ea1ae5bd8
Arjun P. Rosell <rosellarjun@gmail.com>
2024-11-25 22:09:06 +0800

    fixed client side

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -51,31 +51,31 @@
                 </p>
             </div>
         </div>
-        {{--
-            <div
+
+        <div
             class="shadow-xs flex items-center rounded-lg bg-gray-100 p-8 dark:bg-gray-800"
-            >
+        >
             <div
-            class="mr-4 rounded-full bg-blue-100 p-3 text-blue-500 dark:bg-blue-500 dark:text-blue-100"
+                class="mr-4 rounded-full bg-blue-100 p-3 text-blue-500 dark:bg-blue-500 dark:text-blue-100"
             >
-            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-            <path
-            d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
-            ></path>
-            </svg>
+                <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                        d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+                    ></path>
+                </svg>
             </div>
             <div>
-            <p class="text-sm font-medium text-gray-600 dark:text-gray-400">
-            Sales Completed
-            </p>
-            <p
-            class="text-lg font-semibold text-gray-700 dark:text-gray-200"
-            >
-            {{ $completedSales }}
-            </p>
+                <p class="text-sm font-medium text-gray-600 dark:text-gray-400">
+                    Completed
+                </p>
+                <p
+                    class="text-lg font-semibold text-gray-700 dark:text-gray-200"
+                >
+                    {{ $completedSales }}
+                </p>
             </div>
-            </div>
-        --}}
+        </div>
+
         <div
             class="shadow-xs flex items-center rounded-lg bg-gray-100 p-8 dark:bg-gray-800"
         >
@@ -123,6 +123,7 @@
         </div>
         </div>
     --}}
+
     <div class="mb-6 h-full overflow-x-auto">
         <div class="mb-2 px-4 text-lg font-semibold">Top Selling Products</div>
         <table
