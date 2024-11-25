8559b358a9c09f0ee795722e20829fa0ce0d033b
Arjun P. Rosell <rosellarjun@gmail.com>
2024-11-25 11:52:37 +0800

    fixed: home route

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,20 @@ Route::get(
         return view('home');
     }
 );
-// Route::get('/', function () {
-//     if (Auth::check()) {
-//         $user = Auth::user();
-//         if ($user->hasRole('admin')) {
-//             return redirect()->route('admin.index');
-//         } elseif ($user->hasRole('staff')) {
-//             return redirect()->route('staff.index');
-//         } elseif ($user->hasRole('client')) {
-//             return redirect()->route('client.index');
-//         }
-//     }
-//     return redirect()->route('login');
-// });
+Route::get('/', function () {
+    if (Auth::check()) {
+        $user = Auth::user();
+        if ($user->hasRole('admin')) {
+            return redirect()->route('admin.index');
+        } elseif ($user->hasRole('staff')) {
+            return redirect()->route('staff.index');
+        } elseif ($user->hasRole('client')) {
+            return redirect()->route('client.index');
+        }
+    }
+
+    return view('home');
+});
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
